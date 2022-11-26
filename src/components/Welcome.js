1d59fda630177a0428923dc15b271ae579f00726
Amulyas369 <amulyafood369@gmail.com>
2022-11-27 04:48:10 +0530

    done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 // write code for Welcome component here
 const Welcome=()=>{
     <>
-    <h1>Hey ! {this.props.name}</h1>
+    <h1>Hey {this.props.name}!</h1>
     <h2>Welcome to Newton School</h2>
     </>
 }
